3f51ffc091445ae919317e1aa2b246c5392b459c
raghav <raghapb34n@gmail.com>
2025-04-14 22:52:02 +0530

    vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
 
 
@@ -21,6 +22,7 @@ export default function RootLayout({
         className="h-full w-full"
       >
         {children}
+        <Analytics />
       </body>
     </html>
   );
